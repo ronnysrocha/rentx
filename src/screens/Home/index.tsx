b2dc71224a33a834e539ebf6ebbcab54d05acc71
Ronny Rocha <ronnysrocha@gmail.com>
2022-04-09 17:25:29 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { RFValue } from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import {
   Container,
   Header,
@@ -12,6 +14,37 @@ import {
 } from './styles';
 
 export function Home() {
+
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  }
+
+  const carDataTwo = {
+    brand: 'Bugatti',
+    name: 'Premium',
+    rent: {
+      period: 'AO DIA',
+      price: 953,
+    },
+    thumbnail: 'https://i.pinimg.com/originals/ac/e6/3b/ace63b2f6ccd29c7e567e070089fcd5b.png'
+  }
+
+  const carDataThree = {
+    brand: 'Lamborghini',
+    name: 'Revent XF3N',
+    rent: {
+      period: 'AO DIA',
+      price: 626,
+    },
+    thumbnail: 'https://www.pngmart.com/files/16/Convertible-Red-Lamborghini-PNG-Clipart.png'
+  }
+
   return (
     <Container>
       <StatusBar
@@ -30,6 +63,13 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne}/>
+
+      <Car data={carDataTwo}/>
+
+      <Car data={carDataThree}/>
+
     </Container>
   );
 }
