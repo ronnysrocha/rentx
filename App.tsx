4d829c58f42b2727f8d39cd6abe8669b6d9d708c
Ronny Rocha <ronnysrocha@gmail.com>
2022-04-11 22:48:15 -0300

    Interface de Agendamento

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ import {
 } from '@expo-google-fonts/archivo';
 
 
-import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
 
 import theme from './src/styles/theme';
 
@@ -35,7 +35,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   )
 }
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
 
 import speedSvg from '../../assets/speed.svg';
 import accelerationSvg from '../../assets/acceleration.svg';
@@ -25,6 +26,7 @@ import {
   Price,
   About,
   Accessories,
+  Footer,
 } from './styles';
 
 export function CarDetails() {
@@ -68,6 +70,10 @@ export function CarDetails() {
           É um belíssimo carro para quem gosta de acelerar.
         </About>
       </Content>
+
+      <Footer>
+        <Button title="Confirmar"/>
+      </Footer>
     </Container>
   );
 }
